a7276d19047967a52f339aba212b8de6fd740c9d
Binary Genius <SherieC@tlgcohort.com>
2024-06-28 02:04:25 -0400

    Redo initial commit with the correct project structure

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+* Classic Hello World application in Java.
+* This is a standalone application, i.e., it does not require any other application to run.
+ */
+class HelloWorld {
+
+    // starting point or entry point for every Java application
+    public static void main(String[] args) {
+        System.out.println("Hello from IntelliJ!"); // prints to the console
+        System.out.println("Hello from the other side!");
+    }
+}
