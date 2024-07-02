7b5b890288e4421b9f23ca6a0a28c68cdd2f7d91
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-02 08:04:33 -0400

    Added files for the homework WhiteBoard assignment.

---
+++ WhiteBoardModel/src/Sheriempic.java
@@ -0,0 +1,26 @@
+class Sheriempic {
+    // Properties or Attributes with Data Encapsulation
+    private String activeIngredient = "Sheriechandolariem";
+    private int strengthMilligrams = 100;
+    private int strengthMilligrams  = 200;
+    private int strengthMilligrams  = 300;
+    private String sideEffect = "May cause sounds of clucking like a chicken during sleep";
+    private String drugStorage = "Keep refrigerated";
+    private String sigRX= "Take 1 tablet by mouth daily with 8 ounces of water containing olive oil";
+    private String mechanismOfAction = "Sheriechandolariemase binds to leptin receptors in the hypothalamus, which leads to decreased food intake";
+
+
+
+    // Constructors
+    public Sheriempic() {
+
+    }
+
+    // Business or Action Methods
+
+    // Data Validation
+
+    // Data Encapsulation: getters and setters
+
+    // toString() Method
+}
