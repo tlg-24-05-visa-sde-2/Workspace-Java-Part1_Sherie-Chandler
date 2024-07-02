1faa1744ed13f139ef45f2871732da1519dfb86e
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-02 13:46:17 -0400

    Defined and incremented instanceCount, provided read access for getInstanceCount

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -5,17 +5,26 @@
  * note-setters are designed to assign values to fields
  */
 class Television {
+    // class-level ("static") variables-these live in a classwide common area, above the instances.
+    public static final int MIN_VOLUME = 0;
+    public static final int MAX_VOLUME = 100;
+
+    public static int instanceCount = 0;
+
+    // this method is also "up there" in the
     // properties or attributes - "fields" or "instance variables"
     private String brand = "Toshiba"; // Private field to store the brand of the television, defaulting to "Toshiba"
     private int volume = 1; // Private field to store the volume level of the television, defaulting to 1
 
     // constructors
     public Television() {
+        instanceCount++; // instanceCount = instanceCount + 1
         // Default constructor, initializes with default values ("Toshiba" brand, volume level 1)
     }
 
     public Television(String brand) {
-        setBrand(brand); // Constructor to set the brand of the television
+        this();           // delegate to no-arg ctor above for the increment
+        setBrand(brand); // delegate to setter for any validation/conversion it might be doing
     }
 
     public Television(String brand, int volume) {
@@ -51,8 +60,18 @@ class Television {
     }
 
     public void setVolume(int volume) {
-        this.volume = volume; // Setter used to set the volume
+        if (volume >= MIN_VOLUME && volume <= MAX_VOLUME) {
+            this.volume = volume;
+            // Setter used to set the volume
+        } else {
+            System.out.println("Volume must be between " + MIN_VOLUME + " and " + MAX_VOLUME);
+        }
     }
+
+    public static int getInstanceCount() {
+        return instanceCount; // static getter
+    }
+
 
     public String toString() {
         return "Television: " +
