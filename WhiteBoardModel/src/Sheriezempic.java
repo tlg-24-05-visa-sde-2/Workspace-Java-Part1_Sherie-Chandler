6cd20fe0c11d15e3bf103c3bfa78e90a7673b40a
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-03 07:51:14 -0400

    Changed the file name and added display methods for RX.

--- WhiteBoardModel/src/Sheriezempic.java
+++ WhiteBoardModel/src/Sheriezempic.java
@@ -1,33 +1,30 @@
 class Sheriezempic {
-    // Properties or Attributes with Data Encapsulation
+    // Properties or Attributes (with Data Encapsulation)
     private String activeIngredient = "Sheriechandolariem";
     private int strengthMilligrams = 100;
-    private String sideEffect = "May cause sounds of clucking like a chicken during sleep";
-    private String drugStorage = "Keep refrigerated";
+    private String sideEffects = "May cause sounds of clucking like a chicken during sleep";
+    private boolean adverseReactions = "May change body odor.";
     private String sigRX= "Take 1 tablet by mouth daily with 8 ounces of water containing olive oil";
     private String mechanismOfAction = "Sheriechandolariemase binds to leptin receptors in the hypothalamus, which leads to decreased food intake";
 
+    // Methods
 
+    public void displayDrugInfo() {
+        System.out.println("Drug Information:");
+        System.out.println("Active Ingredient:" + activeIngredient);
+        System.out.println("Strength:" + strengthMilligrams + "mg");
+        System.out.println("Patient Instructions: " + sigRX);
+       System.out.println("How does Sheriezempic work?:" + mechanismOfAction);
+    }
+
+    public void sideEffects () { // write an if-else statement that if the adverse reaction happens than a potential side effect will be clucking like a chicken.
+        System.out.println("Potential Side Effect:" + sideEffects);
+
+    }
+
+public void
 
     // Constructors
-    public Sheriezempic() {
-
-    }
-
-    public Sheriezempic(String activeIngredient) {
-        set activeIngredient(activeIngredient);
-
-    }
-
-    public Sheriezempic(String sideEffect) {
-        set sideEffect(sideEffect);
-
-    }
-
-    public Sheriezempic(String mechanismOfAction) {
-        set mechanismOfAction(mechanismOfAction);
-
-
 
     // Business or Action Methods
 
