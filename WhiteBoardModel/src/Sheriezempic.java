df7e93589ba4974ad0ab05ea0c361eb29eaa8152
Sherie Chandler <SherieC@tlgcohort.com>
2024-07-05 08:20:40 -0400

    Added getters and setters

--- WhiteBoardModel/src/Sheriezempic.java
+++ WhiteBoardModel/src/Sheriezempic.java
@@ -1,52 +1,111 @@
-class Sheriezempic
+class Sheriezempic {
     // Properties or Attributes (with Data Encapsulation)
     private String activeIngredient = "Sheriechandolariem";
     private int strengthMilligrams = 100;
     private String sideEffects = "May cause sounds of clucking like a chicken during sleep";
-    private boolean adverseReactions = "May change body odor.";
-    private String sigRX= "Take 1 tablet by mouth daily with 8 ounces of water containing olive oil";
+    private String adverseReaction = "May change body odor to scents of Jasmine, Oud, or Bergamot";
+    private String sigRX = "Take 1 tablet by mouth daily with 8 ounces of water containing olive oil";
     private String mechanismOfAction = "Sheriechandolariemase binds to leptin receptors in the hypothalamus, which leads to decreased food intake";
+    private AdverseReactionSeverity adverseReactionSeverity;
 
     // Methods
-public void displayDrugInfo() {
+    public void displayDrugInfo() {
         System.out.println("Drug Information:");
-        System.out.println("Active Ingredient:" + activeIngredient);
-        System.out.println("Strength:" + strengthMilligrams + "mg");
+        System.out.println("Active Ingredient: " + activeIngredient);
+        System.out.println("Strength: " + strengthMilligrams + "mg");
         System.out.println("Patient Instructions: " + sigRX);
-       System.out.println("How does Sheriezempic work?:" + mechanismOfAction);
+        System.out.println("How does Sheriezempic work?: " + mechanismOfAction);
     }
 
-    public void displaySideEffects () { // write an if-else statement that if the adverse reaction happens than a potential side effect will be clucking like a chicken.
-        System.out.println("Potential Side Effect:" + sideEffects);
-        if (adverseReactionOccured) {
+    public void displaySideEffects() {
+        System.out.println("Potential Side Effect: " + sideEffects);
+        if (adverseReactions) {
             switch (adverseReactionSeverity) {
-                case: Mild
-                        System.out.println("Adverse Reaction Mild: " + adverseReactions);
-                break;
-
-                switch (adverseReactionSeverity) {
-                    case: Moderate
-                        System.out.println("Adverse Reaction Moderate: " + sideEffects);
-                        break;
-
-                    switch (adverseReactionSeverity) {
-                        case: Severe
-                            System.out.println("Adverse Reaction Severe: " + sideEffects + adverseReactions));
-                            break;
+                case Mild:
+                    System.out.println("Adverse Reaction Mild: " + sideEffects);
+                    break;
+                case Moderate:
+                    System.out.println("Adverse Reaction Moderate: " + sideEffects);
+                    break;
+                case Severe:
+                    System.out.println("Adverse Reaction Severe: " + sideEffects + ", " + adverseReactions);
+                    break;
+            }
+        }
     }
-
-public void
 
     // Constructors
-
-    // Business or Action Methods: add enum (double check placement-maybe it should be under your initial class)
-    public enum AdverseReactionSeverity{
-        Mild, Moderate, Severe;
+    public Sheriezempic(AdverseReactionSeverity severity) {
+        this.adverseReactionSeverity = severity;
     }
+
+    // Business or Action Methods
 
     // Data Validation
 
     // Data Encapsulation: getters and setters
+    public String getActiveIngredient() {
+        return activeIngredient;
+    }
 
-    // toString() Method
+    public void setActiveIngredient(String activeIngredient) {
+        this.activeIngredient = activeIngredient;
+    }
+
+    public int getStrengthMilligrams() {
+        return strengthMilligrams;
+    }
+
+    public void setStrengthMilligrams(int strengthMilligrams) {
+        this.strengthMilligrams = strengthMilligrams;
+    }
+
+    public String getSideEffects() {
+        return sideEffects;
+    }
+
+    public void setSideEffects(String sideEffects) {
+        this.sideEffects = sideEffects;
+    }
+
+    public String getAdverseReaction() {
+        return adverseReaction;
+    }
+
+    public void setAdverseReaction(String adverseReaction) {
+        this.adverseReaction = adverseReaction;
+    }
+
+    public String getSigRX() {
+        return sigRX;
+    }
+
+    public void setSigRX(String sigRX) {
+        this.sigRX = sigRX;
+    }
+
+    public String getMechanismOfAction() {
+        return mechanismOfAction;
+    }
+
+    public void setMechanismOfAction(String mechanismOfAction) {
+        this.mechanismOfAction = mechanismOfAction;
+    }
+
+    public AdverseReactionSeverity getAdverseReactionSeverity() {
+        return adverseReactionSeverity;
+    }
+
+    public void setAdverseReactionSeverity(AdverseReactionSeverity adverseReactionSeverity) {
+        this.adverseReactionSeverity = adverseReactionSeverity;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Sheriezempic{activeIngredient='%s', strengthMilligrams=%d, sideEffects='%s', adverseReaction='%s', sigRX='%s', mechanismOfAction='%s', adverseReactionSeverity=%s}",
+                activeIngredient, strengthMilligrams, sideEffects, adverseReaction, sigRX, mechanismOfAction, adverseReactionSeverity);
+    }
+
+}
 }
